8739cc0f5836d1dba9656a97938373aa03a5a27f
hassan <hassanbajwa154@gmail.com>
2024-07-22 18:11:21 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        customBlue: '#1E40AF',
+        customGreen: '#10B981',
+        customRed: '#EF4444',
+      },
+    },
+  },
+  plugins: [],
+}
